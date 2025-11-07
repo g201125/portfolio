b702cc7111d5de4e0c85822aa23f9e745f1162ee
Morqqulis <morqqulis@gmail.com>
2025-11-07 16:49:01 +0400

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import About from "@/components/about/about";
-import { Hero } from "../components/hero/hero";
-import Skills from "@/components/skils/skils";
-import Services1 from "@/components/services1/services1";
+import About from '@/components/about/about'
+import { Hero } from '../components/hero/hero'
+import Skills from '@/components/skils/skils'
+import Services1 from '@/components/services1/services1'
 
-export default function () {
-    return (
-        <main>
-            <Hero />
-            <About />
-            <Skills />
-            <Services1 />
-        </main>
-      
-    )
+export default function Home() {
+	return (
+		<main>
+			<Hero />
+			<About />
+			<Skills />
+			<Services1 />
+		</main>
+	)
 }
--- src/components/skils/skils.tsx
+++ src/components/skils/skils.tsx
@@ -1,53 +1,36 @@
-
-import Image from "next/image";
+import Image from 'next/image'
 
 const Skills = () => {
-    const skills = [
-        { name: "HTML" },
-        { name: "CSS" },
-        { name: "JavaScript" },
-    ];
+	const skills = [
+		{ name: 'HTML', value: 90 },
+		{ name: 'CSS', value: 80 },
+		{ name: 'JS', value: 70 },
+	]
 
-    return (
-        <section className="py-[140px] bg-[#F6F6F8] text-white">
-            <div className="container mx-auto px-4">
-                <h2 className="text-[48px] mb-8 text-black">
-                    Навыки
-                </h2>
+	return (
+		<section className='py-[140px] bg-[#F6F6F8] '>
+			<div className='container mx-auto px-4'>
+				<h2 className='text-[48px] mb-8 text-black'>Навыки</h2>
 
-                <div className="flex flex-col md:flex-row items-center `justify-between gap-10">
+				<div className='flex flex-col md:flex-row items-center justify-between gap-4'>
+					<Image src='/men.png' alt='men' width={415} height={450} className='object-contain' />
 
-                    <Image
-                        src="/men.png"
-                        alt="men"
-                        width={415}
-                        height={450}
-                        className="object-contain"
-                    />
+					<div className='w-full max-w-[670px] space-y-10 md:space-y-[85px]'>
+						{skills.map(skill => (
+							<div className={`flex items-center`} key={skill.name}>
+								<span className={`min-w-[64px] font-semibold`}>{skill.name}</span>
+								<div className={`w-full h-[32px] bg-[#DBDBED] relative`}>
+									<span
+										className={`absolute left-0 h-full bg-[#2352DE] w-full`}
+										style={{ maxWidth: `${skill.value}%` }}></span>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-
-                    <div className="w-full max-w-md space-y-8">
-                        {skills.map((skill) => (
-                            <div key={skill.name}>
-                                <div className="flex justify-between mb-2">
-                                    <span className="text-lg font-medium text-[#333333]">
-                                        {skill.name}
-                                    </span>
-                                    <span className="text-gray-400"></span>
-                                </div>
-                                <div className="w-full h-4">
-                                    <div
-                                        className="bg-blue-600 h-4 transition-all duration-1000 ease-out"
-
-                                    ></div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-};
-
-export default Skills;
+export default Skills
